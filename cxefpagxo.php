5a42c24fa1dc6a6e84b991f854ce79f390ae8c29
Logan HALL "CodeWeaver <codeweaver@loganhall.net>
2022-01-22 18:39:08 -0600

    rimedo por tuj aldoni difinon ĉe la ĉefpaĝo

--- cxefpagxo.php
+++ cxefpagxo.php
@@ -8,18 +8,31 @@
 			content="Via vortaro — la komunume regata fonto enreta de difinoj k rilatoj pri ĉiaj terminoj!">
 		<meta name="keywords" content="esperanto, EO, la popola vortaro, popola, vortaro">
 		<meta name="author" content='Logan HALL "CodeWeaver"'>
-		<meta name="robots" content="index,nofollow">
+		<meta name="robots" content="index,follow">
 	</head>
 	<body>
 		<header style="background-image:linear-gradient(0deg,black,#00CC22)">
-			<h1>LA POPOLA VORTARO</h1>
-			<h3>
-				serĉi <input id="serĉvorto" type="text" list="trafoj">
-				<datalist id="trafoj"></datalist>
-				<button onclick="window.location.assign(document.getElementById('serĉvorto').value);">
-					iri
-				</button>
-			</h3>
+			<section>
+				<h1>LA POPOLA VORTARO</h1>
+				<h3>
+					serĉi <input id="serĉvorto" type="text" list="trafoj">
+					<datalist id="trafoj"></datalist>
+					<button onclick="window.location.assign(document.getElementById('serĉvorto').value);">
+						iri
+					</button>
+				</h3>
+			</section>
+			<hr>
+			<section>
+				<form action="" method="post">
+					<h3>Difinu vorton!</h3>
+					<label for="vorto">termino</label>
+					<input id="vorto" name="vorto" type="text" placeholder="eĥ-o-ŝanĝ-o ĉiu-ĵaŭd-e">
+					<br>
+					<label for="difino">difino:</label>
+					<textarea id="difino" name="difino"></textarea>
+				</form>
+			</section>
 		</header>
 		<hr>
 		<main>
